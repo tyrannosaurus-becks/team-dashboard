e2fa48c933f51492bcaa8b94931624f48860722f
Becca Petrin <beccapetrin@gmail.com>
2021-09-03 17:01:53 -0700

    add local file output

--- internal/dashboards/registry.go
+++ internal/dashboards/registry.go
@@ -5,5 +5,6 @@ import "github.com/tyrannosaurus-becks/team-dashboard/internal/models"
 func All(config *models.Config) []models.Dashboard {
 	return []models.Dashboard{
 		newDatadog(config),
+		&localFile{},
 	}
 }
